9af8454cc609427e655e5aa306e42fc73ff452d1
Matt Phillips <matt@mattphillips.io>
2017-09-05 13:15:22 +0100

    Add either/maybe/task examples

--- example.js
+++ example.js
@@ -1,26 +1,39 @@
 const fetch = require('node-fetch');
-const { identity: id } = require('ramda');
-
-const v1 = require('./src/data.task/');
+const { taskFetch, eitherFetch, maybeFetch } = require('./src/data.task/');
 const v2 = require('./src/folktale/');
 
-const url = 'https://swapi.co/api/people';
+const successUrl = 'https://swapi.co/api/people';
+const errorUrl = 'https://swapi.co/api/people/998221';
 
 const get = async () => {
-  const v1Result = await v1(fetch)(url).fork(id, id);
-  v1Result.fold(
+  console.log('-'.repeat(10), 'TASK', '-'.repeat(10));
+  await taskFetch(fetch)(successUrl).fork(
     console.error,
     console.log
   );
 
-  console.log('-'.repeat(50));
+  console.log('-'.repeat(10), 'MAYBE', '-'.repeat(10));
+  const maybe = await maybeFetch(fetch)(successUrl);
+  maybe.cata({
+    Nothing: () => console.error('Nothing'),
+    Just: console.log
+  });
 
-  try {
-    const v2Result = await v2(fetch)(url).run().promise();
-    console.log(v2Result);
-  } catch (e) {
-    console.error(e);
-  }
+  console.log('-'.repeat(10), 'EITHER', '-'.repeat(10));
+  const either = await eitherFetch(fetch)(successUrl);
+  either.fold(
+    console.error,
+    console.log
+  );
+
+  // console.log('-'.repeat(50));
+
+  // try {
+  //   const v2Result = await v2(fetch)(url).run().promise();
+  //   console.log(v2Result);
+  // } catch (e) {
+  //   console.error(e);
+  // }
 };
 
 get();
